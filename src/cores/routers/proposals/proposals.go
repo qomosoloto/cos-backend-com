ee35809f2f5e283b5efb147cf67e7acca163650a
Paul <musong0058@sina.com>
2021-05-24 00:28:23 +0800

    update

--- src/cores/routers/proposals/proposals.go
+++ src/cores/routers/proposals/proposals.go
@@ -2,6 +2,7 @@ package proposals
 
 import (
 	"cos-backend-com/src/common/flake"
+	"cos-backend-com/src/common/validate"
 	"cos-backend-com/src/cores/routers"
 	"cos-backend-com/src/libs/apierror"
 	"cos-backend-com/src/libs/models/proposalmodels"
@@ -23,6 +24,32 @@ func (h *ProposalsHandler) GetProposal(id flake.ID) (res interface{}) {
 		res = apierror.HandleError(err)
 		return
 	}
+
+	res = apires.With(&output, http.StatusOK)
+	return
+}
+
+func (h *ProposalsHandler) ListProposals() (res interface{}) {
+	var params cores.ListProposalsInput
+	h.Params.BindValuesToStruct(&params)
+
+	if err := validate.Default.Struct(params); err != nil {
+		h.Log.Warn(err)
+		res = apierror.HandleError(err)
+		return
+	}
+
+	var uid flake.ID
+	h.Ctx.Find(&uid, "uid")
+
+	var output cores.ListProposalsResult
+	total, err := proposalmodels.Proposals.ListProposals(h.Ctx, uid, &params, &output.Result)
+	if err != nil {
+		h.Log.Warn(err)
+		res = apierror.HandleError(err)
+		return
+	}
+	output.Total = total
 
 	res = apires.With(&output, http.StatusOK)
 	return
